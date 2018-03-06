6e4574072ee6e190a371450ac93e35ef02e51307
Colin But <colinbut@users.noreply.github.com>
2018-03-06 10:56:13 +0000

    Sending script

---
+++ marketing_email_sender.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'bunny'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+
+queue = channel.queue('email-queue')
+
+channel.default_exchange.publish('test message', routing_key: queue.name)
+puts ' [x] Sent message to RabbitMQ'
+
+connection.close
